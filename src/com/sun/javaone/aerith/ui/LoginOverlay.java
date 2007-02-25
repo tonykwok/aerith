f502b6162965ffa5ad836af05f921f1edcdb184b
gfx <gfx@1617aa94-c031-4d33-b1e1-f5b53684d216>
2007-02-25 15:13:01 +0000

    Fixed AA bug in login screen

--- src/com/sun/javaone/aerith/ui/LoginOverlay.java
+++ src/com/sun/javaone/aerith/ui/LoginOverlay.java
@@ -12,6 +12,7 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.Point;
 import java.awt.Rectangle;
+import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.BorderFactory;
@@ -251,6 +252,8 @@ public class LoginOverlay extends JComponent implements ActionListener {
         super.paintComponent(g);
 
         Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
         paintBackground(g2);
     }
     
